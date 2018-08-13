357d720100818b4fb99bfd9f70c18a38404d1563
Josh Jaslow <jbossjaslow@gmail.com>
2018-08-13 11:45:50 -0400

    Initial Commit

---
+++ sceneKitMacOs/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  sceneKitMacOs
+//
+//  Created by Josh Jaslow on 6/17/17.
+//  Copyright (c) 2017 Jaslow Enterprises. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
